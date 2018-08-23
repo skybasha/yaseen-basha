833aa1a5017b67938a5f4e4d463ec28456113532
skybasha <41729735+skybasha@users.noreply.github.com>
2018-08-23 11:05:03 +0530

    Create prog214.java

---
+++ prog214.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		for( n=1;n<m;n++)
+		while((n%2)!=0)
+		
+			
+		{
+		System.out.print(n+" ");
+		break;
+		}	
+		
+			
+		
+		
+	}
+}
