a3903f31bcd6f4216b561b014a164443afe15c95
skybasha <41729735+skybasha@users.noreply.github.com>
2018-08-29 12:19:10 +0530

    Create prog31.java

---
+++ prog31.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int word=0;
+	Scanner sc=new Scanner(System.in);
+	String n=sc.nextLine();
+	for(int i=0;i<n.length();i++)
+	{
+	if(n.charAt(i)==' ')
+	{
+		continue;
+		
+	}
+	word++;
+	}
+	System.out.print(word);
+	}
+}
