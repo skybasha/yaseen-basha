190b0cb07dc09cb584b15ad11293bcdaa581cb34
skybasha <41729735+skybasha@users.noreply.github.com>
2018-09-05 14:05:53 +0530

    Create prog37.java

---
+++ prog37.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int x, y, temp;
+      Scanner sc = new Scanner(System.in);
+ 
+      x = sc.nextInt();
+      y = sc.nextInt();
+ 
+      System.out.println(x+" "+y);
+ 
+      temp = x;
+      x = y;
+      y = temp;
+ 
+      System.out.println(x+" "+y);
+	}
+}
